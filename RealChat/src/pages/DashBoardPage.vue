1a4a93356435ce7b139ee5ed0c476213880191b6
Miguel <miguelvascomacamo@gmail.com>
2018-01-27 20:15:52 +0200

    criando o modelo de env.example

--- RealChat/src/pages/DashBoardPage.vue
+++ RealChat/src/pages/DashBoardPage.vue
@@ -1,11 +1,13 @@
 <template>
-    $END$
+    <div class="card-wrapper" id="dashboard-wrapper">
+      <p class="info-color">Bem Vindo</p>
+    </div>
 </template>
 
 <script>
-    export default {
-        name: "dash-board-page"
-    }
+export default {
+  name: 'dash-board-page'
+}
 </script>
 
 <style scoped>
